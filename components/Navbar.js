496521010429afd42cfcc35ca78ac3833feac176
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2022-02-15 10:17:33 +0700

    App Saver

--- components/Navbar.js
+++ components/Navbar.js
@@ -137,6 +137,32 @@ export default function Navbar() {
               )}
             </a>
           </Link>
+          <Link href="/saver">
+            <a
+              className={`text-base  ${
+                router.asPath === "/saver"
+                  ? "text-gray-800 font-bold dark:text-gray-400"
+                  : "text-gray-600 dark:text-gray-300 font-normal "
+              }`}
+            >
+              Saver{" "}
+              {router.asPath === "/saver" && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-arrow-down inline-block h-3 w-3"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+                  />
+                </svg>
+              )}
+            </a>
+          </Link>
         </div>
 
         <div className="space-x-4 flex flex-row items-center">
@@ -238,6 +264,11 @@ export default function Navbar() {
         <Link href="/bear">
           <a className="text-base font-normal text-gray-600 dark:text-gray-300">
             Bear
+          </a>
+        </Link>
+        <Link href="/saver">
+          <a className="text-base font-normal text-gray-600 dark:text-gray-300">
+            Saver
           </a>
         </Link>
       </div>
---
+++ pages/saver.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ContainerBlock from "../components/ContainerBlock";
+import Saver from "@components/Saver";
+
+export default function about() {
+  return (
+    <ContainerBlock>
+      <Saver />
+    </ContainerBlock>
+  );
+}
